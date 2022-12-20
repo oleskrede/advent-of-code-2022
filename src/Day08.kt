c78fb595a5104892f8b12d7b7539cb5afb8e7618
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2022-12-20 20:10:32 +0100

    aoc22 day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,60 +1,74 @@
 fun main() {
 
-    class Tree(
+    val maxTreeHeight = 9
+
+    data class Tree(
         val height: Int,
-        var visible: Boolean = false
+        var visible: Boolean = false,
+        val viewingDistances: MutableList<Int> = mutableListOf(),
     )
 
-
-    fun updateVisibilityForTreeLine(trees: List<Tree>) {
-        var highest = -1
-        for (tree in trees) {
-            if (tree.height > highest) {
-                highest = tree.height
-                tree.visible = true
-            }
-        }
-    }
-
-
-    fun part1(input: List<String>): Int {
+    class Forest(input: List<String>) {
         val trees = input.map { line ->
             line.map { height ->
                 Tree(height.digitToInt())
             }
         }
 
-        for (horizontalLine in trees) {
-            updateVisibilityForTreeLine(horizontalLine)
-            updateVisibilityForTreeLine(horizontalLine.reversed())
+        init {
+            for (horizontalLine in trees) {
+                updateTreeValuesForTreeLine(horizontalLine)
+                updateTreeValuesForTreeLine(horizontalLine.reversed())
+            }
+
+            val transposedTrees = trees.flatten()
+                .withIndex()
+                .groupBy { it.index % trees.size }
+                .map { indexed -> indexed.value.map { it.value } }
+
+            for (verticalLine in transposedTrees) {
+                updateTreeValuesForTreeLine(verticalLine)
+                updateTreeValuesForTreeLine(verticalLine.reversed())
+            }
         }
 
-        val transposedTrees = trees.flatten()
-            .withIndex()
-            .groupBy { it.index % trees.size }
-            .map { indexed -> indexed.value.map { it.value } }
-
-        for (verticalLine in transposedTrees) {
-            updateVisibilityForTreeLine(verticalLine)
-            updateVisibilityForTreeLine(verticalLine.reversed())
+        // Stupid function name for a function that does the grunt work of solving part 1 and 2
+        fun updateTreeValuesForTreeLine(trees: List<Tree>) {
+            var highest = -1
+            val viewingDistances = MutableList(10) { 0 } // pos = height, value = position
+            for ((position, tree) in trees.withIndex()) {
+                if (tree.height > highest) {
+                    highest = tree.height
+                    tree.visible = true
+                }
+                val positionsOfHigherTrees = viewingDistances.takeLast(maxTreeHeight - tree.height + 1)
+                val viewingDistance = position - positionsOfHigherTrees.max()
+                tree.viewingDistances.add(viewingDistance)
+                viewingDistances[tree.height] = position
+            }
         }
+    }
 
-        return trees.flatten().count { it.visible }
+
+    fun part1(input: List<String>): Int {
+        val forest = Forest(input)
+        return forest.trees.flatten().count { it.visible }
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val forest = Forest(input)
+        return forest.trees.flatten().map { tree ->
+            tree.viewingDistances.fold(1) { t, r ->
+                t * r
+            }
+        }.max()
     }
 
     val testInput = readInput("Day08_test")
     test(part1(testInput), 21)
-    test(part2(testInput), 1)
+    test(part2(testInput), 8)
 
     val input = readInput("Day08")
     timedRun { part1(input) }
     timedRun { part2(input) }
-}
-
-private enum class Direction(val dx: Int, val dy: Int) {
-    UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGHT(1, 0)
 }
