23e4fde5240e8f74330c2d29f8df4a5260f4ab04
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2022-12-26 16:15:42 +0100

    add skeleton files for day 10-24

---
+++ src/Day10.kt
@@ -0,0 +1,21 @@
+import kotlin.math.abs
+import kotlin.math.max
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val testInput = readInput("Day10_test")
+    test(part1(testInput), 1)
+    test(part2(testInput), 1)
+
+    val input = readInput("Day09")
+    timedRun { part1(input) }
+    timedRun { part2(input) }
+}
