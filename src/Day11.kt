a3484088e5a4200011335ac10a6c888adc2c1ad6
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2023-01-15 16:44:26 +0100

    aoc22 day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -4,17 +4,18 @@ fun main() {
         val id: Int,
         val items: MutableList<Long>,
         val operation: (Long) -> Long,
+        val handleWorryLevel: (Long) -> Long,
         val test: (Long) -> Int,
         val monkies: List<Monkey>,
     ) {
-        var activityCounter = 0
+        var activityCounter = 0L
 
         fun takeTurn() {
             while (items.isNotEmpty()) {
                 activityCounter++
                 var worryLevel = items.removeAt(0)
                 worryLevel = operation(worryLevel)
-                worryLevel /= 3
+                worryLevel = handleWorryLevel(worryLevel)
                 val receivingMonkeyId = test(worryLevel)
                 monkies[receivingMonkeyId].items.add(worryLevel)
             }
@@ -31,39 +32,58 @@ fun main() {
         }
     }
 
-    fun part1(input: List<String>): Int {
+    fun handleMonkeyBusiness(input: List<String>, numRounds: Int, handleWorryLevel: (Long) -> Long): Long {
         val monkies = mutableListOf<Monkey>()
-        input.filter { !it.isBlank() }.windowed(size = 6, step = 6).forEach { monkeyInfo ->
+        var commonFactor = 1L
+        input.filter { it.isNotBlank() }.windowed(size = 6, step = 6).forEach { monkeyInfo ->
             val id = monkeyInfo[0].removeSuffix(":").split(" ").last().toInt()
             val startingItems = monkeyInfo[1].split(": ").last().split(", ").map { it.toLong() }
             val operationTokens = monkeyInfo[2].split(" ").takeLast(2)
             val operation = toOperation(operationTokens[0], operationTokens[1])
             val testValue = monkeyInfo[3].split(" ").last().toLong()
+            commonFactor *= testValue
             val testTrueMonkey = monkeyInfo[4].split(" ").last().toInt()
             val testFalseMonkey = monkeyInfo[5].split(" ").last().toInt()
             val test =
                 { worryLevel: Long -> if (worryLevel % testValue == 0L) testTrueMonkey else testFalseMonkey }
-            val monkey = Monkey(id, startingItems.toMutableList(), operation, test, monkies)
+            val monkey = Monkey(id, startingItems.toMutableList(), operation, handleWorryLevel, test, monkies)
             monkies.add(monkey)
         }
 
-        repeat(20) {
+        repeat(numRounds) {
             monkies.forEach { monkey -> monkey.takeTurn() }
         }
 
         val sortedActivityCounters = monkies.map { it.activityCounter }.sortedDescending()
-        val monkeyBusinesss = sortedActivityCounters[0] * sortedActivityCounters[1]
 
-        return monkeyBusinesss
+        return sortedActivityCounters[0] * sortedActivityCounters[1]
     }
 
-    fun part2(input: List<String>): Int {
-        return 1
+    fun part1(input: List<String>): Long {
+        return handleMonkeyBusiness(
+            input = input,
+            numRounds = 20,
+            handleWorryLevel = { wl: Long -> wl / 3 }
+        )
+    }
+
+    fun part2(input: List<String>): Long {
+
+        val commonFactor = input
+            .filter { it.contains("divisible by") }
+            .map { it.split(" ").last().toLong() }
+            .reduce { acc, testValue -> acc * testValue }
+
+        return handleMonkeyBusiness(
+            input = input,
+            numRounds = 10_000,
+            handleWorryLevel = { wl: Long -> wl % commonFactor }
+        )
     }
 
     val testInput = readInput("Day11_test")
-    test(part1(testInput), 10605)
-    test(part2(testInput), 1)
+    test(part1(testInput), 10605L)
+    test(part2(testInput), 2713310158L)
 
     val input = readInput("Day11")
     timedRun { part1(input) }
